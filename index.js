4537c0ddf436e8ddc09f4bd29b80f66dbac6fd2f
louisyhz <83959681+louisyhz@users.noreply.github.com>
2021-07-07 21:00:20 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+
+let fn = document.getElementById('fullname');
+let em = document.getElementById('email');
+let msg = document.getElementById('message');
+let fm = document.getElementById('contact');
+let regEm = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+
+function formValidator(ev) {
+
+    ev.preventDefault();
+
+    let errors = [];
+    let data = {};
+
+    if (fn.value !== '') {
+        data.fullName = fn.value;
+    } else {
+        errors.push('Full name is missing!');
+    }
+
+
+    if (em.value !== '') {
+    if (regEm.test(em.value)) {
+        data.em = em.value;  
+        console.log("This is a valid email address.");
+      }else {
+          console.log("This is an invalid email address.");
+          
+      }
+    }else {
+        errors.push('Email is missing!');
+    }
+ 
+  
+    if (msg.value !== '') {
+        data.message = msg.value;
+    } else {
+        errors.push('Message is missing!');
+    }
+    
+
+    if (errors.length == 0 ) {
+        console.log('Collected Data');
+        console.log(data);
+        document.getElementById('contact').reset();
+    }
+    else {
+        console.log('Errors');
+        console.log(errors);
+    }
+}
+
+fm.addEventListener('submit', formValidator);
+
+
+
+
